1cb84f5903310fa7886407a66e3c27295f878821
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2022-10-27 14:54:30 -0300

    décimo capitulo completado

---
+++ capitulo_10/RWData.java
@@ -0,0 +1,9 @@
+// Grava e depois lê dados binários
+
+import java.io.*;
+
+class RWData {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ capitulo_10/SpaceToHifen2.java
@@ -0,0 +1,48 @@
+/* Este programa copia um arquivo de texto e converte todos os
+ * espaçoes em hifens.
+ */
+
+import java.io.*;
+
+class SpaceToHifen {
+    public static void main(String[] args)
+    throws IOException {
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        // Verifica se os dois arquivos foram especificados
+        if(args.length != 2) {
+            System.out.println("Usage: java SpaceToHifen <original file> <destination file>");
+            return;
+        }
+
+        //Copia um arquivo e substitui os espaços por hifens
+        try {
+            // Tenta abrir os arquivos
+            fin = new FileInputStream(args[0]);
+            fout = new FileOutputStream(args[1]);
+
+            do {
+                i = fin.read();
+                if (i != -1) {
+                    if ((char) i == ' ') i = (char) '-';
+                    fout.write(i);
+                }
+            } while (i != -1);
+        } catch (IOException exc) {
+            System.out.println("I/O Exception: " + exc);
+        } finally {
+            try {
+                if (fin != null) fin.close();
+            }catch(IOException exc) {
+                System.out.println("error closing input file!");
+            }
+            try {
+                if (fout != null) fout.close();
+            } catch (IOException exc) {
+                System.out.println("error closing output file!");
+            }
+        }
+    }
+}
